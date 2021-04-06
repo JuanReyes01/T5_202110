1204efa0725b179c40dc815d28d578c2a7c914a1
Juan Camilo Reyes <j.reyesc@uniandes.edu.co>
2021-04-06 11:26:53 -0500

    fork

---
+++ README.txt
@@ -0,0 +1,2 @@
+Juan Camilo Reyes - 201922989
+Tales Losada - 201819986
---
+++ src/model/data_structures/ITablaSimbolos.java
@@ -0,0 +1,12 @@
+package model.data_structures;
+
+public interface ITablaSimbolos<K extends Comparable<K>,V> {
+	void put(K k, V v);
+	V get(K k);
+	V remove(K k);
+	boolean contains (K k);
+	boolean isEmpty ( );
+	int size ( );
+	ILista<K> keySet();
+	ILista<V> valueSet();
+}
